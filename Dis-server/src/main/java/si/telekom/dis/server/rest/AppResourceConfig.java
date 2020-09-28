1ac2c4c53d8aeca8e5acb4ece26b5196adc97309
zhivko <klemen.zivkovic@gmail.com>
2020-09-28 07:15:57 +0200

    rest

--- Dis-server/src/main/java/si/telekom/dis/server/rest/AppResourceConfig.java
+++ Dis-server/src/main/java/si/telekom/dis/server/rest/AppResourceConfig.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 //Deployment of a JAX-RS application using @ApplicationPath with Servlet 3.0
 //Descriptor-less deployment
 import org.glassfish.jersey.server.ResourceConfig;
@@ -15,7 +16,8 @@ public class AppResourceConfig extends ResourceConfig {
 	public AppResourceConfig() {
 
 		register(new LoggingFeature(java.util.logging.Logger.getGlobal(), java.util.logging.Level.ALL, LoggingFeature.Verbosity.PAYLOAD_ANY, 10000));
-
+		register(MultiPartFeature.class);
+		
     // Tracing properties (modification of the response HTTP headers)
     Map<String, Object> params = new HashMap<String, Object>();
     params.put("jersey.config.server.tracing.type","ALL");
@@ -25,7 +27,8 @@ public class AppResourceConfig extends ResourceConfig {
 		packages("si.telekom.dis.server.rest");
 		ResourceConfig config = new ResourceConfig(DisRest.class);
 		config.register(LoggingFeature.class);
-    
+		
+
 		
 		// config.register(AuthenticationFilter.class);
 	}
--- Dis-server/src/main/java/si/telekom/dis/server/rest/DisRest.java
+++ Dis-server/src/main/java/si/telekom/dis/server/rest/DisRest.java
@@ -1,14 +1,15 @@
 package si.telekom.dis.server.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import si.telekom.dis.server.AdminServiceImpl;
@@ -18,6 +19,7 @@ import si.telekom.dis.shared.Profile;
 // https://erender-test.ts.telekom.si:8445/Dis/rest/disRest/dqlLookup?loginName=zivkovick&passwordEncrypted=RG9pdG1hbjc4OTAxMg==&dql=select%20*%20from%20dm_cabinet
 // http://localhost:8080/Dis-server/rest/disRest/dqlLookup?loginName=zivkovick&passwordEncrypted=RG9pdG1hbjc4OTAxMg==&dql=select%20*%20from%20dm_cabinet
 // http://localhost:8080/Dis-server/rest/application.wadl
+@Named
 @Path("/disRest")
 public class DisRest {
 
@@ -31,17 +33,27 @@ public class DisRest {
 	@Path("/dqlLookup")
 	public List<List<String>> dqlLookup(@QueryParam("loginName") String loginName, @QueryParam("passwordEncrypted") String passwordEncrypted,
 			@QueryParam("dql") String dql) throws Exception {
-		return ExplorerServiceImpl.getInstance().dqlLookup(loginName, passwordEncrypted, dql);
+		// return ExplorerServiceImpl.getInstance().dqlLookup(loginName,
+		// passwordEncrypted, dql);
+		try {
+			return ExplorerServiceImpl.getInstance().dqlLookup(loginName, passwordEncrypted, dql);
+		} catch (Exception ex) {
+			throw new WebApplicationException(ex.getMessage());
+		}
 	}
 
 	@GET
-	@Consumes(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("/newDocument")
 	public String newDocument(@QueryParam("loginName") String loginName, @QueryParam("password") String password,
 			@QueryParam("profileId") String profileId, @QueryParam("attributes") Map<String, List<String>> attributes, Map<String, List<String>> rolesUsers,
-			String templateObjectNameOrFolder) throws Exception {
-		return ExplorerServiceImpl.getInstance().newDocument(loginName, password, profileId, attributes, rolesUsers, templateObjectNameOrFolder);
+			String templateObjectNameOrFolder) {
+		try {
+			return ExplorerServiceImpl.getInstance().newDocument(loginName, password, profileId, attributes, rolesUsers, templateObjectNameOrFolder);
+		} catch (Exception ex) {
+			throw new WebApplicationException(ex.getMessage());
+		}
 	}
 
 	@GET
@@ -51,23 +63,28 @@ public class DisRest {
 	public String importDocument(@QueryParam("loginName") String loginName, @QueryParam("password") String password,
 			@QueryParam("folderRObjectId") String folderRObjectId, @QueryParam("profileId") String profileId,
 			@QueryParam("attributes") Map<String, List<String>> attributes, @QueryParam("rolesUsers") Map<String, List<String>> rolesUsers,
-			@QueryParam("base64Content") byte[] base64Content, @QueryParam("format") String format) throws Exception {
-		return ExplorerServiceImpl.getInstance().importDocument(loginName, password, folderRObjectId, profileId, attributes, rolesUsers, base64Content,
-				format);
+			@QueryParam("base64Content") byte[] base64Content, @QueryParam("format") String format) {
+		// return ExplorerServiceImpl.getInstance().importDocument(loginName,
+		// password, folderRObjectId, profileId, attributes, rolesUsers,
+		// base64Content,
+		// format);
+		try {
+			return ExplorerServiceImpl.getInstance().importDocument(loginName, password, folderRObjectId, profileId, attributes, rolesUsers, base64Content,
+					format);
+		} catch (Exception ex) {
+			throw new WebApplicationException(ex.getMessage());
+		}
+
 	}
 
 	@GET
 	@Consumes(MediaType.TEXT_HTML)
 	@Produces({ MediaType.APPLICATION_JSON })
 	@Path("/getProfilesForClassSign")
-	public List<String> getProfilesForClassSign(@QueryParam("loginName") String loginName, @QueryParam("passwordEncrypted") String passwordEncrypted,
+	public List<Profile> getProfilesForClassSign(@QueryParam("loginName") String loginName, @QueryParam("passwordEncrypted") String passwordEncrypted,
 			@QueryParam("classSign") String classSign, @QueryParam("wizardType") String wizardType) {
 		List<Profile> profiles = AdminServiceImpl.getInstance().getProfilesForClassSign(loginName, passwordEncrypted, classSign, wizardType);
-		ArrayList<String> ret = new ArrayList<String>();
-		for (Profile prof : profiles) {
-			ret.add(prof.id);
-		}
-		return ret;
+		return profiles;
 	}
 
 	@GET
