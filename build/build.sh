ee637bcc3894bc4474d6e212f4b3b23f1b47a04a
zivkovick <klemen.zivkovic@telekom.si>
2021-09-21 15:02:38 +0200

    cicd

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,2 @@
-ln -s /src/main/filters/test.properties /Dis-shared/src/main/filters/test.properties
-ln -s /src/main/filters/dev.properties /Dis-shared/src/main/filters/dev.properties
-ln -s /src/main/filters/prod.properties /Dis-shared/src/main/filters/prod.properties
-
-ln -s /src/main/filters/test.properties /Dis-client/src/main/filters/test.properties
-ln -s /src/main/filters/dev.properties /Dis-client/src/main/filters/dev.properties
-ln -s /src/main/filters/prod.properties /Dis-client/src/main/filters/prod.properties
-
-ln -s /src/main/filters/test.properties /Dis-server/src/main/filters/test.properties
-ln -s /src/main/filters/dev.properties /Dis-server/src/main/filters/dev.properties
-ln -s /src/main/filters/prod.properties /Dis-server/src/main/filters/prod.properties
+mvn test -P test -Dgwt.skipCompilation=true
 
